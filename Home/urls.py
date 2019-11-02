f7ad54ca612ff27d3d62a4f491cd49ed9d1c6506
James Wang <james.wangyu81@gmail.com>
2019-11-02 12:11:08 -0500

    add upload view

--- Home/urls.py
+++ Home/urls.py
@@ -5,4 +5,5 @@ app_name = "home"
 
 urlpatterns = [
     path('test/', views.test_view, name='test_view'),
+    path('upload_video/', views.upload_file, name='upload_video'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def test_view(request):
     return render(request, template_name='Home/test.html')
 
+
+def upload_file(request):
+    if request.method == 'POST':
+        pass
+    else: # get
+        return render(request, template_name='Home/upload.html')
