c47dd976f7b7db7416370cd2bd699952dbefde60
James Wang <james.wangyu81@gmail.com>
2019-11-02 14:33:49 -0500

    fix issues

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Videos(models.Model):
+    #user = models.ManyToManyField(User, default=1, on_delete=models.SET_DEFAULT)
+    user = models.ForeignKey(User, default=1, verbose_name='User', on_delete=models.SET_DEFAULT)
+    video_filename = models.FileField(upload_to='Videos/')
+
+    def __str__(self):
+        return '{} {}'.format(self.user.username, self.video_filename)
+
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Videos
 
 
 # Create your views here.
@@ -8,6 +10,12 @@ def test_view(request):
 
 def upload_file(request):
     if request.method == 'POST':
-        pass
-    else: # get
+        user_obj = User.objects.all()[:1].get()
+        upload = request.POST.get('upload')
+        if upload is None:
+            return redirect('home:upload_video')
+        v = Videos.objects.create(user=user_obj, video_filename=upload)
+        v.save()
+        return redirect('home:upload_video')
+    else:  # get
         return render(request, template_name='Home/upload.html')
